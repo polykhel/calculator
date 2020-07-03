e7349874feb7168ffbf17109e6be08cc6a7bac9c
Michael Pollente <mpollente@exist.com>
2020-07-03 21:50:06 +0800

    Fix acceptance test script

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-CALCULATOR_PORT=${docker-compose port calculator 8080 | cut -d: -f2}
+CALCULATOR_PORT=$(docker-compose port calculator 8080 | cut -d: -f2)
 ./gradlew acceptanceTest -Dcalculator.url="http://localhost:${CALCULATOR_PORT}"
